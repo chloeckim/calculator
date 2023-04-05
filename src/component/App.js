75b53b6fcd9cf0c1690d41893ecfc58ff42ccfdf
Chloe <chloeckim0717@gmail.com>
2023-04-05 16:46:38 -0700

    Add components.

--- src/component/App.js
+++ src/component/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
+import ButtonPanel from './ButtonPanel';
+import Display from './Display';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="component-app">
+      <Display value='0'/>
+      <ButtonPanel clickHandler={() => {}} />
     </div>
   );
 }
